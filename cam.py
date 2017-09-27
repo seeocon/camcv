9c425a449578d5994d6a05039af5290dd9d84a8e
Michael O'Connor <miichael.oconnor@gmail.com>
2017-09-27 15:29:39 -0400

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,37 +1,100 @@
-import io
-import picamera
-import cv2
-import numpy
+import sys
+import cv2.cv as cv
+from optparse import OptionParser
 
-#Create a memory stream so photos doesn't need to be saved in a file..
-stream = io.BytesIO()
+min_size = (20, 20)
+image_scale = 2
+haar_scale = 1.2
+min_neighbors = 2
+haar_flags = 0
 
-#Get the picture (low resolution, so it should be quite fast)
-#Here you can also specify other parameters (e.g.:rotate the image)
-with picamera.PiCamera() as camera:
-    camera.resolution = (320, 240)
-    camera.capture(stream, format='jpeg')
+def detect_and_draw(img, cascade):
+# allocate temporary images
+gray = cv.CreateImage((img.width,img.height), 8, 1)
+small_img = cv.CreateImage((cv.Round(img.width / image_scale),
+cv.Round (img.height / image_scale)), 8, 1)
 
-#Convert the picture into a numpy array
-buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
+# convert color input image to grayscale
+cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
 
-#Now creates an OpenCV image
-image = cv2.imdecode(buff, 1)
+# scale input image for faster processing
+cv.Resize(gray, small_img, cv.CV_INTER_LINEAR)
+cv.EqualizeHist(small_img, small_img)
 
-#Load a cascade file for detecting faces
-face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
+if(cascade):
+t = cv.GetTickCount()
+faces = cv.HaarDetectObjects(small_img, cascade, cv.CreateMemStorage(0),
+haar_scale, min_neighbors, haar_flags, min_size)
+t = cv.GetTickCount() – t
+print “time taken for detection = %gms” % (t/(cv.GetTickFrequency()*1000.))
+if faces:
+for ((x, y, w, h), n) in faces:
+# the input to cv.HaarDetectObjects was resized, so scale the
+# bounding box of each face and convert it to two CvPoints
+pt1 = (int(x * image_scale), int(y * image_scale))
+pt2 = (int((x + w) * image_scale), int((y + h) * image_scale))
+cv.Rectangle(img, pt1, pt2, cv.RGB(255, 0, 0), 3, 8, 0)
 
-#Convert to grayscale
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+cv.ShowImage(“video”, img)
 
-#Look for faces in the image using the loaded cascade file
-faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+if __name__ == ‘__main__’:
 
-print "Found "+str(len(faces))+" face(s)"
+parser = OptionParser(usage = “usage: %prog [options] [filename|camera_index]”)
+parser.add_option(“-c”, “–cascade”, action=”store”, dest=”cascade”, type=”str”, help=”Haar cascade file, default %default”, default = “../data/haarcascades/haarcascade_frontalface_alt.xml”)
+(options, args) = parser.parse_args()
 
-#Draw a rectangle around every found face
-for (x,y,w,h) in faces:
-    cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+cascade = cv.Load(options.cascade)
 
-#Save the result image
-cv2.imwrite('result.jpg',image)
+if len(args) != 1:
+parser.print_help()
+sys.exit(1)
+
+input_name = args[0]
+if input_name.isdigit():
+capture = cv.CreateCameraCapture(int(input_name))
+else:
+capture = None
+
+cv.NamedWindow(“video”, 1)
+
+#size of the video
+width = 160
+height = 120
+
+if width is None:
+width = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH))
+else:
+cv.SetCaptureProperty(capture,cv.CV_CAP_PROP_FRAME_WIDTH,width)
+
+if height is None:
+height = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT))
+else:
+cv.SetCaptureProperty(capture,cv.CV_CAP_PROP_FRAME_HEIGHT,height)
+
+if capture:
+frame_copy = None
+while True:
+
+frame = cv.QueryFrame(capture)
+if not frame:
+cv.WaitKey(0)
+break
+if not frame_copy:
+frame_copy = cv.CreateImage((frame.width,frame.height),
+cv.IPL_DEPTH_8U, frame.nChannels)
+
+if frame.origin == cv.IPL_ORIGIN_TL:
+cv.Copy(frame, frame_copy)
+else:
+cv.Flip(frame, frame_copy, 0)
+
+detect_and_draw(frame_copy, cascade)
+
+if cv.WaitKey(10) >= 0:
+break
+else:
+image = cv.LoadImage(input_name, 1)
+detect_and_draw(image, cascade)
+cv.WaitKey(0)
+
+cv.DestroyWindow(“video”)
